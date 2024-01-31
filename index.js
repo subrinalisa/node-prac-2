9ac175ab0cd11a3d890f9e9604e1755a1f96e6df
Subrinalisa <subrinalisa5@example.com>
2024-01-31 10:56:23 +0600

    cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+var cors = require("cors");
 const areaRouter = require("./routes/area.route");
-const app = express();
 const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(areaRouter);
 
